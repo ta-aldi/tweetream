e4cc3f1cbc40f6a9f0dbc7523267573455c57b87
Michael Susanto <michael.susanto81@ui.ac.id>
2021-09-02 10:53:15 +0700

    add sample cloud-based kafka, testing producer and consumer from local

---
+++ sample_kafka/consumer.py
@@ -0,0 +1,46 @@
+from confluent_kafka import Consumer
+
+def commit_completed(err, partitions):
+    if err:
+        print(str(err))
+    else:
+        print("Committed partition offsets: " + str(partitions))
+
+conf = {'bootstrap.servers': "35.239.209.233:9092",
+        'group.id': "foo",
+        'default.topic.config': {'auto.offset.reset': 'smallest'},
+        'on_commit': commit_completed}
+
+consumer = Consumer(conf)
+
+# Client
+running = True
+
+def msg_process(msg):
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+def basic_consume_loop(consumer, topics):
+    try:
+        consumer.subscribe(topics)
+
+        while running:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None: continue
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                     (msg.topic(), msg.partition(), msg.offset()))
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                msg_process(msg)
+    finally:
+        # Close down consumer to commit final offsets.
+        consumer.close()
+
+def shutdown():
+    running = False
+
+basic_consume_loop(consumer, ['TutorialTopic'])
