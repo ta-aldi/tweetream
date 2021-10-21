0ad191d4a06cde0cc6a675cf1efd13c12e235e68
Michael Susanto <michael.susanto81@ui.ac.id>
2021-10-21 16:13:13 +0700

    disable GET method

--- modules/streamer-server.py
+++ modules/streamer-server.py
@@ -2,11 +2,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
         return {'msg': 'Hello World POST'}
-    return {'msg': 'Hello World'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
