a5d0b55bc4c38e308d3609246f5437b96636df78
Michael Susanto <michael.susanto81@ui.ac.id>
2021-10-26 19:17:01 +0700

    send information about streamer server into frontend

--- webclient/controllers/controller.go
+++ webclient/controllers/controller.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"log"
+	"os"
+
 	"github.com/beego/beego/v2/server/web"
+	"github.com/joho/godotenv"
 	"github.com/michaelsusanto81/tweetream/webclient/services"
 )
 
@@ -13,6 +17,15 @@ func (c *MainController) Get() {
 	c.TplName = "index.html"
 	Response := services.GetTopics()
 	c.Data["topics"] = Response
+
+	// load .env credentials
+	err_env := godotenv.Load(".env")
+	if err_env != nil {
+		log.Fatalf(err_env.Error())
+	}
+
+	// pass streamer server IP address information
+	c.Data["STREAMER_SERVER"] = os.Getenv("STREAMER_SERVER")
 }
 
 func (c *MainController) GetTopics() {
