4e9c03cc48ca20d2c437f1310eccbd17db516621
Michael Susanto <michael.susanto81@ui.ac.id>
2021-11-04 10:57:19 +0700

    add stage load test

--- tests/main.py
+++ tests/main.py
@@ -63,15 +63,33 @@ class TweetreamUser(HttpUser, SocketIOUser):
 
 
 class TweetreamShape(LoadTestShape):
-    time_limit = 600
-    spawn_rate = 10
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+    Keyword arguments:
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages = [
+        {"duration": 60, "users": 10, "spawn_rate": 10},
+        {"duration": 100, "users": 50, "spawn_rate": 10},
+        {"duration": 180, "users": 100, "spawn_rate": 10},
+        {"duration": 220, "users": 30, "spawn_rate": 10},
+        {"duration": 230, "users": 10, "spawn_rate": 10},
+        {"duration": 240, "users": 1, "spawn_rate": 1},
+    ]
 
     def tick(self):
         run_time = self.get_run_time()
 
-        if run_time < self.time_limit:
-            # User count rounded to nearest hundred.
-            user_count = round(run_time, -2)
-            return (user_count, self.spawn_rate)
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
 
         return None
