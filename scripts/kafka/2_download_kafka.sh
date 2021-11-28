da630a543b726f0fc98481a9ba92784609638092
Michael Susanto <michael.susanto81@ui.ac.id>
2021-11-28 20:58:33 +0700

    update download url for kafka

--- scripts/kafka/2_download_kafka.sh
+++ scripts/kafka/2_download_kafka.sh
@@ -6,6 +6,6 @@
 # This script using Kafka 2.8.0 (Scala 2.13)
 echo "Downloading Kafka Binary . . ."
 mkdir ~/Downloads
-curl "https://dlcdn.apache.org/kafka/2.8.0/kafka_2.13-2.8.0.tgz" -o ~/Downloads/kafka.tgz
+curl "https://archive.apache.org/dist/kafka/2.8.0/kafka_2.13-2.8.0.tgz" -o ~/Downloads/kafka.tgz
 mkdir ~/kafka && cd ~/kafka
 tar -xvzf ~/Downloads/kafka.tgz --strip 1
